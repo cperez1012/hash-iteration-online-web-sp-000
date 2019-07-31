1a496d0fb148ff1cd4f083de6553d0d5ad806bb0
Christian Perez <perezchristian1012@gmail.com>
2019-07-31 20:56:35 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,10 +1,10 @@
-passengers = {
-suite_a: ["Amanda Presley", "Barry Johnson", "Alex Perez"],
-suite_b: "Seymour Hoffman",
-suite_c: "Alfred Tennyson",
-suite_d: "Charlie Chaplin",
-suite_e: "Crumpet the Elf"
-}
+# passengers = {
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
+# suite_e: "Crumpet the Elf"
+# }
 require 'pry'
 def select_winner(passengers)
   # add the code snippet here!
