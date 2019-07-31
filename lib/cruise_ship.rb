744886aa21930bbcb1d067aadcb219ea692d15dc
Christian Perez <perezchristian1012@gmail.com>
2019-07-31 20:45:23 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,19 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
-
+passengers = {
+suite_a: ["Amanda Presley", "Barry Johnson", "Alex Perez"],
+suite_b: "Seymour Hoffman",
+suite_c: "Alfred Tennyson",
+suite_d: "Charlie Chaplin",
+suite_e: "Crumpet the Elf"
+}
+require 'pry'
 def select_winner(passengers)
   # add the code snippet here!
+passengers[:suite_a].map do |name|
+  if name.start_with?("A")
+    name
+
+  end
+end.compact.join(", ")
+
 end
+#select_winner(passengers)
