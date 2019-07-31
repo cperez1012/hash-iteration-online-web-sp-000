957aef320e4faf9360d5db51a1ce5c4d4e1e0c6c
Christian Perez <perezchristian1012@gmail.com>
2019-07-31 20:57:49 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,12 +8,7 @@
 require 'pry'
 def select_winner(passengers)
   # add the code snippet here!
-passengers[:suite_a].map do |name|
-  if name.start_with?("A")
-    name
-
-  end
-end.compact.join(", ")
+passengers[:suite_a]
 
 end
 #select_winner(passengers)
